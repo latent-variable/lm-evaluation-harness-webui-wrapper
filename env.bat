61d5cb2bbb06ffda1111ea69adb16570da57a086
latent_variable <linoa7x@hotmail.com>
2023-08-11 03:19:42 -0700

    working on windows support

---
+++ env.bat
@@ -0,0 +1,2 @@
+call C:\AI\oobabooga-windows\installer_files\conda\Scripts\activate.bat && conda activate C:\AI\oobabooga-windows\installer_files\env
+pause
---
+++ run_eval.bat
@@ -0,0 +1,2 @@
+call activate_env.bat
+python run_eval.py
